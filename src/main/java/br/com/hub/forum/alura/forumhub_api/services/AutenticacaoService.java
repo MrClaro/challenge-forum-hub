9f1d0d1d609c941bf4641b36b4996dd841e49702
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-05 23:00:11 -0300

    feat(service): criado service para implementação das regras de autenticação do projeto

---
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/services/AutenticacaoService.java
@@ -0,0 +1,36 @@
+package br.com.hub.forum.alura.forumhub_api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Service;
+
+import br.com.hub.forum.alura.forumhub_api.domain.dto.autenticacao.DadosAutenticacao;
+import br.com.hub.forum.alura.forumhub_api.domain.entity.Usuario;
+
+@Service
+public class AutenticacaoService {
+
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private TokenService tokenService;
+
+  public String autenticar(DadosAutenticacao dados) {
+    try {
+      var authenticationToken = new UsernamePasswordAuthenticationToken(
+          dados.email(),
+          dados.senha());
+
+      var authentication = authenticationManager.authenticate(authenticationToken);
+
+      var usuario = (Usuario) authentication.getPrincipal();
+      return tokenService.gerarToken(usuario);
+
+    } catch (AuthenticationException e) {
+      throw new IllegalArgumentException("Credenciais inválidas");
+    }
+  }
+}
