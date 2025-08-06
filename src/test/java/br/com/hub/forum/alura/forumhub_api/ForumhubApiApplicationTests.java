3ca364c9116b3e03e6f18c4a6ff6416c0a938a56
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-05 22:52:12 -0300

    feat(init): configurações padrões geradas pelo spring boot

---
+++ src/test/java/br/com/hub/forum/alura/forumhub_api/ForumhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hub.forum.alura.forumhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
