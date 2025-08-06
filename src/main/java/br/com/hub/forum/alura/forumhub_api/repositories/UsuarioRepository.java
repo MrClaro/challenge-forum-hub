31fd881d87a78a8e6ad6e3975ee3af1d85d0c4d0
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-05 22:58:49 -0300

    feat(repository): criado repository de usuarios extendendo o JpaRepository

---
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/repositories/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package br.com.hub.forum.alura.forumhub_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import br.com.hub.forum.alura.forumhub_api.domain.entity.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+  UserDetails findByEmail(String email);
+
+}
