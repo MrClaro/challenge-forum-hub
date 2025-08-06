c1204032b5a3c3faee233d642362e416b6bf1801
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-05 22:54:04 -0300

    feat: classe inicial do projeto gerada pelo spring boot

---
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/ForumhubApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.hub.forum.alura.forumhub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumhubApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ForumhubApiApplication.class, args);
+  }
+
+}
