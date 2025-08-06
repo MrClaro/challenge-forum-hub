c217ab9a0c78012fedcc77c4dbfac2c7b734d0fc
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-05 22:54:45 -0300

    feat(model): criado classe para entidade de Topicos

---
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/domain/entity/Topico.java
@@ -0,0 +1,78 @@
+package br.com.hub.forum.alura.forumhub_api.domain.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import br.com.hub.forum.alura.forumhub_api.domain.entity.enums.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "topicos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  private String titulo;
+
+  private String mensagem;
+
+  @Enumerated(EnumType.STRING)
+  private Status status;
+
+  private Boolean ativo;
+
+  @ManyToOne
+  @JoinColumn(name = "autor_id")
+  private Usuario autor;
+
+  @ManyToOne
+  @JoinColumn(name = "curso_id")
+  private Curso curso;
+
+  @OneToMany(mappedBy = "topico", fetch = FetchType.LAZY)
+  private List<Resposta> respostas;
+
+  @Column(name = "data_criacao")
+  private LocalDateTime dataCriacao;
+
+  @Column(name = "data_atualizacao")
+  private LocalDateTime dataAtualizacao;
+
+  @Column(name = "data_exclusao")
+  private LocalDateTime dataExclusao;
+
+  @PrePersist
+  protected void onCreate() {
+    dataCriacao = LocalDateTime.now();
+    dataAtualizacao = LocalDateTime.now();
+  }
+
+  @PreUpdate
+  protected void onUpdate() {
+    dataAtualizacao = LocalDateTime.now();
+  }
+
+}
