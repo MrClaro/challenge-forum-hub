c2f933946f2f9fd199db196cf379739000bdb633
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-07 20:55:59 -0300

    refactor(controller): refatorado controller de autenticação para seguir as boas práticas do spring boot

--- src/main/java/br/com/hub/forum/alura/forumhub_api/controllers/AutenticacaoController.java
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/controllers/AutenticacaoController.java
@@ -13,7 +13,6 @@ import br.com.hub.forum.alura.forumhub_api.domain.dto.autenticacao.DadosAutentic
 import br.com.hub.forum.alura.forumhub_api.domain.dto.autenticacao.DadosRegistroComToken;
 import br.com.hub.forum.alura.forumhub_api.domain.dto.autenticacao.DadosRegistroPublico;
 import br.com.hub.forum.alura.forumhub_api.domain.dto.autenticacao.DadosTokenJWT;
-import br.com.hub.forum.alura.forumhub_api.infra.exception.ErrorResponse;
 import br.com.hub.forum.alura.forumhub_api.services.AutenticacaoService;
 import br.com.hub.forum.alura.forumhub_api.services.UsuarioService;
 import jakarta.validation.Valid;
@@ -29,37 +28,22 @@ public class AutenticacaoController {
   private UsuarioService usuarioService;
 
   @PostMapping("/login")
-  public ResponseEntity<?> login(@RequestBody @Valid DadosAutenticacao dados) {
-    try {
-      var token = autenticacaoService.autenticar(dados);
-      return ResponseEntity.ok(new DadosTokenJWT(token));
-    } catch (Exception e) {
-      var errorResponse = new ErrorResponse("Credenciais inválidas", "Email ou senha incorretos");
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorResponse);
-    }
+  public ResponseEntity<DadosTokenJWT> login(@RequestBody @Valid DadosAutenticacao dados) {
+    var token = autenticacaoService.autenticar(dados);
+    return ResponseEntity.ok(new DadosTokenJWT(token));
   }
 
   @PostMapping("/registrar")
-  @Transactional
-  public ResponseEntity<?> registrarPublico(@RequestBody @Valid DadosRegistroPublico dados) {
-    try {
-      var dadosComCargo = dados.toDadosCadastroUsuario();
+  public ResponseEntity<DadosRegistroComToken> registrarPublico(@RequestBody @Valid DadosRegistroPublico dados) {
+    var dadosComCargo = dados.toDadosCadastroUsuario();
 
-      var usuarioCadastrado = usuarioService.cadastrarUsuario(dadosComCargo);
+    var usuarioCadastrado = usuarioService.cadastrarUsuario(dadosComCargo);
 
-      var dadosAuth = new DadosAutenticacao(dados.email(), dados.senha());
-      var token = autenticacaoService.autenticar(dadosAuth);
+    var dadosAuth = new DadosAutenticacao(dados.email(), dados.senha());
+    var token = autenticacaoService.autenticar(dadosAuth);
 
-      var resposta = new DadosRegistroComToken(usuarioCadastrado, token);
-      return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
-
-    } catch (IllegalArgumentException e) {
-      var errorResponse = new ErrorResponse("Conflito de dados", e.getMessage());
-      return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
-    } catch (Exception e) {
-      var errorResponse = new ErrorResponse("Erro interno", "Erro inesperado ao registrar usuário");
-      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
-    }
+    var resposta = new DadosRegistroComToken(usuarioCadastrado, token);
+    return ResponseEntity.status(HttpStatus.CREATED).body(resposta);
   }
 
 }
