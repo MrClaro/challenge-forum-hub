52e8e44f1f8c14b097ba5c9a972d79bf09452e0a
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-07 20:54:25 -0300

    feat(repository): implementado metodos para obtenção de dados seguindo as regras de negocio da aplicação

--- src/main/java/br/com/hub/forum/alura/forumhub_api/repositories/UsuarioRepository.java
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/repositories/UsuarioRepository.java
@@ -1,5 +1,7 @@
 package br.com.hub.forum.alura.forumhub_api.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -8,5 +10,11 @@ import br.com.hub.forum.alura.forumhub_api.domain.entity.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, String> {
 
   UserDetails findByEmail(String email);
+
+  Page<Usuario> findAllByAtivoTrue(Pageable paginacao);
+
+  boolean existsByEmailAndIdNot(String email, String id);
+
+  boolean existsByEmail(String email);
 
 }
