51def023fd527c2cf7056d308f146844e98187c8
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-07 20:49:13 -0300

    feat_refator(controller): criado metodos de atualização e delete de usuários / refatorado controller de usuários de modo a seguir as boas praticas do spring boot

--- src/main/java/br/com/hub/forum/alura/forumhub_api/controllers/UsuarioController.java
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/controllers/UsuarioController.java
@@ -1,46 +1,71 @@
 package br.com.hub.forum.alura.forumhub_api.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.hub.forum.alura.forumhub_api.domain.dto.usuario.DadosAtualizacaoUsuario;
 import br.com.hub.forum.alura.forumhub_api.domain.dto.usuario.DadosCadastroUsuario;
-import br.com.hub.forum.alura.forumhub_api.infra.exception.ErrorResponse;
+import br.com.hub.forum.alura.forumhub_api.domain.dto.usuario.DadosDetalhamentoUsuario;
 import br.com.hub.forum.alura.forumhub_api.services.UsuarioService;
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("usuarios")
+@RequestMapping("/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
 
-  @Autowired
-  private UsuarioService usuarioService;
+  private final UsuarioService usuarioService;
 
   @PostMapping
-  @Transactional
-  @PreAuthorize("hasRole('ADMIN')")
-  public ResponseEntity<?> cadastrarUsuario(@RequestBody @Valid DadosCadastroUsuario dados,
+  public ResponseEntity<DadosDetalhamentoUsuario> cadastrarUsuario(@RequestBody @Valid DadosCadastroUsuario dados,
       UriComponentsBuilder uriBuilder) {
-    try {
-      var usuarioCadastrado = usuarioService.cadastrarUsuario(dados);
-      var uri = uriBuilder
-          .path("/usuarios/{id}")
-          .buildAndExpand(usuarioCadastrado.id())
-          .toUri();
+    var usuarioCadastrado = usuarioService.cadastrarUsuario(dados);
+    var uri = uriBuilder
+        .path("/usuarios/{id}")
+        .buildAndExpand(usuarioCadastrado.id())
+        .toUri();
 
-      return ResponseEntity.created(uri).body(usuarioCadastrado);
-    } catch (IllegalArgumentException e) {
-      var errorResponse = new ErrorResponse("Conflito de dados",
-          e.getMessage());
-      return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
-    }
+    return ResponseEntity.created(uri).body(usuarioCadastrado);
   }
 
+  @GetMapping
+  public ResponseEntity<Page<DadosDetalhamentoUsuario>> listarUsuarios(
+      @PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+    var usuarios = usuarioService.listarUsuarios(paginacao);
+    return ResponseEntity.ok(usuarios);
+
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<DadosDetalhamentoUsuario> listarUsuarioPorId(@PathVariable String id) {
+    var usuario = usuarioService.obterPorId(id);
+    return ResponseEntity.ok(usuario);
+  }
+
+  @PatchMapping("/{id}")
+  public ResponseEntity<DadosDetalhamentoUsuario> atualizarUsuario(
+      @PathVariable String id,
+      @RequestBody @Valid DadosAtualizacaoUsuario dados) {
+    var usuarioAtualizado = usuarioService.atualizarUsuario(id, dados);
+    return ResponseEntity.ok(usuarioAtualizado);
+
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deletarUsuario(@PathVariable String id) {
+    usuarioService.deletarUsuario(id);
+    return ResponseEntity.noContent().build();
+  }
 }
