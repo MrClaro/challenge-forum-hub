da20276dd74c06a16cc5277dacb1b551f500d2ab
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-13 01:07:25 -0300

    delete: removido linha não utilizada

--- src/main/java/br/com/hub/forum/alura/forumhub_api/controllers/AutenticacaoController.java
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/controllers/AutenticacaoController.java
@@ -3,7 +3,6 @@ package br.com.hub.forum.alura.forumhub_api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
