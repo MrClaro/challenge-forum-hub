2955a5c411e26f67078f004ae55fe7b5b1d553e8
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-13 01:12:35 -0300

    feat(model): adicionado novos métodos para para pre criação e pre update do model de matricula

--- src/main/java/br/com/hub/forum/alura/forumhub_api/domain/entity/Matricula.java
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/domain/entity/Matricula.java
@@ -2,7 +2,9 @@ package br.com.hub.forum.alura.forumhub_api.domain.entity;
 
 import java.time.LocalDateTime;
 
+import br.com.hub.forum.alura.forumhub_api.domain.dto.matricula.DadosAtualizacaoMatricula;
 import br.com.hub.forum.alura.forumhub_api.domain.entity.enums.StatusMatricula;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -11,7 +13,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -36,13 +40,56 @@ public class Matricula {
   @JoinColumn(name = "curso_id")
   private Curso curso;
 
+  @Column(name = "data_matricula")
   private LocalDateTime dataMatricula;
+  @Column(name = "data_conclusao")
   private LocalDateTime dataConclusao;
 
   @Enumerated(EnumType.STRING)
   private StatusMatricula status;
   private double progresso;
 
+  @Column(name = "avaliacao_aluno")
   private Integer avaliacaoAluno;
   private String comentario;
+
+  @PrePersist
+  protected void onCreate() {
+    dataMatricula = LocalDateTime.now();
+  }
+
+  public Matricula(Usuario usuario, Curso curso) {
+    this.usuario = usuario;
+    this.curso = curso;
+    this.status = StatusMatricula.ATIVO;
+    this.progresso = 0.0;
+  }
+
+  public void cancelar() {
+    this.status = StatusMatricula.CANCELADO;
+  }
+
+  public void reativar() {
+    this.status = StatusMatricula.ATIVO;
+  }
+
+  public void atualizarDados(@Valid DadosAtualizacaoMatricula dados) {
+    if (dados.status() != null) {
+      this.status = dados.status();
+    }
+    if (dados.progresso() > 0 && dados.progresso() < 100) {
+      this.progresso = dados.progresso();
+    }
+    if (dados.avaliacaoAluno() != null) {
+      this.avaliacaoAluno = dados.avaliacaoAluno();
+    }
+    if (dados.comentario() != null) {
+      this.comentario = dados.comentario();
+    }
+    if (dados.progresso() == 100) {
+      this.dataConclusao = LocalDateTime.now();
+      this.status = StatusMatricula.CONCLUIDO;
+      this.progresso = 100.0;
+    }
+  }
 }
