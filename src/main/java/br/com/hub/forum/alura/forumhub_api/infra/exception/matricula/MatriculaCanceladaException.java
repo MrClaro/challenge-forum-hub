ecc7ee84ce27536d164e9f8b8221f5f71fc1e8b3
MrClaro <adryan.contatoprofissional@gmail.com>
2025-08-13 01:20:53 -0300

    feat(infra_exception): criado exception para matricula cancelada

---
+++ src/main/java/br/com/hub/forum/alura/forumhub_api/infra/exception/matricula/MatriculaCanceladaException.java
@@ -0,0 +1,7 @@
+package br.com.hub.forum.alura.forumhub_api.infra.exception.matricula;
+
+public class MatriculaCanceladaException extends RuntimeException {
+  public MatriculaCanceladaException(String message) {
+    super(message);
+  }
+}
